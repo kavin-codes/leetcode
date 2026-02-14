0c096b4ec411ef47bbdf6759d7165ffb9bc144c7
kavin-codes <thekavindev@gmail.com>
2026-02-14 17:50:40 +0530

    3-LongestSubstringWithoutRepeatingCharacters/medium/.py

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -9,7 +9,7 @@ class Solution:
 
         for right in range(len(s)):
             while s[right] in char_set:
-                char_set.remove(s[right])
+                char_set.remove(s[left])
                 left+=1
             char_set.add(s[right])
             max_len=max(max_len, right-left+1)
