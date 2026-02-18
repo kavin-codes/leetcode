8efaa0a9a0d20d4b5e07c0104e49243c81b96fb0
kavin-codes <thekavindev@gmail.com>
2026-02-18 22:04:54 +0530

    67-TwoSumII-InputArrayIsSorted/medium/.py

---
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -0,0 +1,22 @@
+nums = [ 2, 3, 4, 11]
+target = 9
+
+class Solution:
+    def single(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            current_sum = nums[left] + nums[right]
+
+            if current_sum == target:
+                return (nums[left], nums[right])
+            elif current_sum > target:
+                right -= 1
+            else:
+                left += 1
+
+        return None  
+
+s1 = Solution()
+print(s1.single(nums, target))
